cc26c4864501d14bf2f1d4a4466be75a56fe7dba
寫詩羊 <Habuche@gmail.com>
2022-02-19 23:28:41 +0800

    style: use noto serif TC

--- pages/neighbor/[id].tsx
+++ pages/neighbor/[id].tsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Neighbor() {
   return (
     <div>
-      我是內頁
+      <div>預設字型</div>
+      <div style={{fontSize: 100}}>我是內頁</div>
+
+      <div>特別字 font-family: 'Noto Serif TC'</div>
+      <a style={{fontSize: 100}}>我是內頁</a>
     </div>
   )
 }
