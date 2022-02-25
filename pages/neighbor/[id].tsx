13e968db9b66cd953732797e51ed59daaefdb6f1
寫詩羊 <Habuche@gmail.com>
2022-02-25 08:46:11 +0800

    updating

--- pages/neighbor/[id].tsx
+++ pages/neighbor/[id].tsx
@@ -46,12 +46,12 @@ const Home: NextPage<Props> = ({ allNeighborsData, neighborData }) => {
   return (
     <Layout>
       <Head>
-        <title>{neighborData.build.toUpperCase()} - {neighborData.sc} {neighborData.name}的完整政見</title>
-        <meta name="description" content={`富貴莊園第二屆管委員候選人${neighborData.name}的完整政見`} />
+        <title>{neighborData.build.toUpperCase()} - {neighborData.sc} {neighborData.name}的經營理念</title>
+        <meta name="description" content={`富貴莊園第二屆管委員候選人${neighborData.name}的經營理念`} />
 
         <meta property="og:url" content={`https://fg2.vercel.app/neighbor/${neighborData.id}`} />
-        <meta property="og:title" content={`${neighborData.build.toUpperCase()} - ${neighborData.sc} ${neighborData.name}的完整政見`} />
-        <meta property="og:description" content={`富貴莊園第二屆管委員候選人${neighborData.name}的完整政見`} />
+        <meta property="og:title" content={`${neighborData.build.toUpperCase()} - ${neighborData.sc} ${neighborData.name}的經營理念`} />
+        <meta property="og:description" content={`富貴莊園第二屆管委員候選人${neighborData.name}的經營理念`} />
         <meta property="og:image" content={neighborData.image} />
 
         <link rel="icon" href="/favicon.ico" />
